0da1496c82eae69b78bf77a15152be8862166fb5
artichoks <quenchelm@gmail.com>
2015-06-02 20:00:57 +0300

    Создан info_providers.h
    - В info_providers.h перечислены прототипы функций отображения различной
    системной информации.

---
+++ info_providers.h
@@ -0,0 +1,12 @@
+#ifndef INFO_PROVIDERS_H_
+#define INFO_PROVIDERS_H_
+
+
+extern void print_os_version();
+extern void print_cpu_info();
+extern void print_memory_info();
+extern void print_display_info();
+extern void print_mount_info();
+
+
+#endif /* INFO_PROVIDERS_H_ */
